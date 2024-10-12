690ee77829a7762d5937276eb849b4981b378187
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-12 19:39:58 +0800

    comments

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::hint::unreachable_unchecked;
 
 
 
-fn main() {
+fn main() { // declare main method
 
 
-    unsafe fn character_to_integer(character: char) -> u8 {
+    unsafe fn character_to_integer(character: char) -> u8 { //decalre unsafe function -> takes parameter character -> DT char -> return unsigned 8 bit
 
-        match character {
+        match character { //match case takes -> character 
             '0' => 0,
             '1' => 1,
             '2' => 2,
